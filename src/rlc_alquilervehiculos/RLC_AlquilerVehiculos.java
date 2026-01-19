eed1a505011ac2af14d92d53fa63b7094cfedeea
dam1 <dam1@AULA44-PC21>
2026-01-19 21:11:45 +0100

    Vehiculo hecho, Cliente creado

--- src/rlc_alquilervehiculos/RLC_AlquilerVehiculos.java
+++ src/rlc_alquilervehiculos/RLC_AlquilerVehiculos.java
@@ -15,6 +15,7 @@ public class RLC_AlquilerVehiculos {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
     }
     
 }
---
+++ src/rlc_alquilervehiculos/Vehiculo.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rlc_alquilervehiculos;
+
+/**
+ *
+ * @author dam1
+ */
+public class Vehiculo {
+    // Atributos
+    private String matricula, marca, modelo;
+    private int cilindrada;
+    private boolean disponible;
+    
+    // Constructores
+    Vehiculo(String matricula, String marca, String modelo, int cilindrada)
+    {
+        this.matricula = matricula;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.cilindrada = cilindrada;
+    }
+    
+    Vehiculo(Vehiculo otroVehiculo) // Constructor copia
+    {
+        this.matricula = otroVehiculo.matricula;
+        this.marca = otroVehiculo.marca;
+        this.modelo = otroVehiculo.modelo;
+        this.cilindrada = otroVehiculo.cilindrada;
+    }
+    
+    // Metodos
+    
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+    
+    public String getMatricula() 
+    {
+        return(matricula);
+    }
+    
+    public String getMarca() 
+    {
+        return(marca);
+    }
+    
+    public String getModelo() 
+    {
+        return(modelo);
+    }
+    
+    public int getCilindrada() 
+    {
+        return(cilindrada);
+    }
+    
+    public boolean isDisponible() 
+    {
+        return(disponible);
+    }
+    
+    @Override
+    public String toString()
+    {
+        return( this.getClass().getSimpleName() +
+                "\nMatricula: " + matricula +
+                "\nMarca: " + marca + 
+                "\nModelo: " + modelo +
+                "\nCilindrada: " + cilindrada +
+                "\nDisponible: " + disponible);
+    }
+}
