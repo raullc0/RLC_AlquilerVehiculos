2c83dabb71ed603d99b13c1526b643936f01e142
dam1 <dam1@AULA44-PC21>
2026-01-21 18:32:28 +0100

    Alquiler Vehiculos: terminar metodos

--- src/rlc_alquilervehiculos/RLC_AlquilerVehiculos.java
+++ src/rlc_alquilervehiculos/RLC_AlquilerVehiculos.java
@@ -10,6 +10,19 @@ package rlc_alquilervehiculos;
  */
 public class RLC_AlquilerVehiculos {
 
+    // Atributos
+    
+    private final int MAX_VEHICULOS = 5;
+    private final int MAX_CLIENTES = 5;
+    private final int MAX_ALQUILERES = 5;
+    private Vehiculo [] vehiculo;
+    
+    private Alquiler [] alquileres;
+    private Cliente [] clientes;
+    
+    
+    // Main
+    
     /**
      * @param args the command line arguments
      */
@@ -17,5 +30,49 @@ public class RLC_AlquilerVehiculos {
         // TODO code application logic here
         
     }
+    
+    // Metodos EDITAR
+    
+    private static Cliente getCliente(String cliente) 
+    {
+        return new Cliente("ohufoqu", "fojhof", "lfnq", "nofn", "nfloqbe");
+    }
+    
+    private static void anadirCliente(Cliente nuevoCliente) 
+    {
+        
+    }
+    
+    private static void borrarCliente(String blabla) 
+    {
+    
+    }
+    
+    private static Vehiculo getVehiculo(String vehiculo)
+    {
+        return new Vehiculo("bla", "ble", "bli", 120);
+    }
+    
+    private static void anadirVehiculo(Cliente nuevoVehiculo) 
+    {
+        
+    }
+    
+    private static void borrarVehiculo(String blabla) 
+    {
+        
+    }
+    
+    private static void nuevoAlquiler(Cliente cliente, Vehiculo vehiculo)
+    {
+    
+    }
+    
+    private static void cerrarAlquiler(Cliente cliente, Vehiculo vehiculo)
+    {
+    
+    }
+    
+    
     
 }
